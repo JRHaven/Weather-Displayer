73c637131a42a44c75ffe21ac3af0f6c54521865
JR-Tech-and-Software <havenchild2@gmail.com>
2025-03-10 20:50:34 -0500

    Replaced too much oops

--- Logger.py
+++ Logger.py
@@ -1,5 +1,5 @@
 '''
-self file is part of Weather-Displayer.
+This file is part of Weather-Displayer.
 
 Weather-Displayer is free software: you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published by the Free 
